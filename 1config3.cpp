978c005695cb60c254b42497989006a957174808
cuckoo <peisonguo@gmail.com>
2016-10-01 20:39:17 +0800

    update 1config3.cpp

--- 1config3.cpp
+++ 1config3.cpp
@@ -7,43 +7,46 @@
 //
 
 #include <iostream>
+#include <string>
 using namespace std;
 
 template<typename T>
 class testClass
 {
 	public:
-		static char _data;
+		static string _data;
 };
 
 // initialization of static data member in a template class
 // need total template specialization
 template<>
-char testClass<int>::_data = 'i';
+string testClass<int>::_data = "int";
 
 template<>
-char testClass<char>::_data = 'c';
+string testClass<char>::_data = "char";
 
 int main()
 {
-	std::cout << testClass<int>::_data << std::endl;
-	std::cout << testClass<char>::_data << std::endl;
+	std::cout << "testClass<int>::_data\t" << testClass<int>::_data << std::endl;
+	std::cout << "testClass<char>::_data\t" << testClass<char>::_data << std::endl;
 
+	std::cout << "==========================" << std::endl;
 	testClass<int> obji1, obji2;
 	testClass<char> objc1, objc2;
 
-	std::cout << obji1._data << std::endl;
-	std::cout << obji2._data << std::endl;
-	std::cout << objc1._data << std::endl;
-	std::cout << objc2._data << std::endl;
+	std::cout << "obji1._data\t" << obji1._data << std::endl;
+	std::cout << "obji2._data\t" << obji2._data << std::endl;
+	std::cout << "objc1._data\t" << objc1._data << std::endl;
+	std::cout << "objc2._data\t" << objc2._data << std::endl;
 
-	obji1._data = 'j';
-	objc2._data = 'h';
+	std::cout << "====== re-assignment =====" << std::endl;
+	obji1._data = "newint";
+	objc2._data = "newchar";
 
-	std::cout << obji1._data << std::endl;
-	std::cout << obji2._data << std::endl;
-	std::cout << objc1._data << std::endl;
-	std::cout << objc2._data << std::endl;
+	std::cout << "obji1._data\t" << obji1._data << std::endl;
+	std::cout << "obji2._data\t" << obji2._data << std::endl;
+	std::cout << "objc1._data\t" << objc1._data << std::endl;
+	std::cout << "objc2._data\t" << objc2._data << std::endl;
 
 	return 0;
 }
