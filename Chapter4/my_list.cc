eb294dedde3a89e40621d18d5165db9cb0db2d22
cuckoo <peisonguo@gmail.com>
2016-11-04 21:32:30 +0800

    new member function

--- Chapter4/my_list.cc
+++ Chapter4/my_list.cc
@@ -12,6 +12,9 @@ using std::endl;
 
 #include "my_list.h"
 
+#include <algorithm>
+using std::find;
+
 int main()
 {
 	MyList<int> ilst;
@@ -60,6 +63,42 @@ int main()
 	cout << endl;
 
 	ilst.clear();
+	for(auto iter = ilst.begin(); iter != ilst.end(); ++iter)
+	{
+		cout << *iter << " ";
+	}
+	cout << endl;
+
+
+	ilst.push_back(0);
+	ilst.push_back(1);
+	ilst.push_back(9);
+
+	MyList<int> ilst2;
+	ilst2.push_back(2);
+	ilst2.push_back(3);
+	ilst2.push_back(4);
+	ilst2.push_back(5);
+	ilst2.push_back(6);
+	ilst2.push_back(7);
+	ilst2.push_back(8);
+
+	MyList<int>::iterator iter = find(ilst.begin(), ilst.end(), 9);
+	ilst.splice(iter, ilst2);
+	for(auto iter = ilst.begin(); iter != ilst.end(); ++iter)
+	{
+		cout << *iter << " ";
+	}
+	cout << endl;
+
+	ilst.reverse();
+	for(auto iter = ilst.begin(); iter != ilst.end(); ++iter)
+	{
+		cout << *iter << " ";
+	}
+	cout << endl;
+
+	ilst.sort();
 	for(auto iter = ilst.begin(); iter != ilst.end(); ++iter)
 	{
 		cout << *iter << " ";
--- Chapter4/my_list.h
+++ Chapter4/my_list.h
@@ -9,7 +9,6 @@
 #ifndef SGI_CHAPTER4_MY_LIST_H_
 #define SGI_CHAPTER4_MY_LIST_H_
 
-#include "Chapter2/my_construct.h"
 
 #include <cstddef>		// for ptrdiff_t, size_t
 #include <bits/stl_iterator_base_types.h>		// for std::bidirectional_iterator_tag
@@ -19,6 +18,7 @@
 using std::cout;
 using std::endl;
 
+#include "Chapter2/my_construct.h"
 #include "Chapter2/default_malloc_template.h"
 
 template <typename T>
@@ -240,6 +240,31 @@ public:
 		return next_node;
 	}
 
+	void splice(iterator position, MyList& x)		// inside the scope of a class template, the complier
+	{												// treats references to the template itself as if we
+		if(!x.empty())								// had supplied template arguments matching the temp-
+		{											// late's own parameters(p588, Primer).
+			transfer(position, x.begin(), x.end());
+		}
+	}
+	void splice(iterator position, MyList&, iterator i)
+	{
+		iterator j = i;
+		++j;
+		if(position == i || position == j) return;
+		transfer(position, i, j);
+	}
+	void splice(iterator position, iterator first, iterator last)
+	{
+		if(first != last)
+			transfer(position, first, last);
+	}
+
+	void reverse();
+	void sort();
+	void merge(MyList &x);
+	void swap(MyList &x) { std::swap(blank_node, x.blank_node); }
+
 protected:
 	// tool function
 	static size_t distance(list_node_ptr first, list_node_ptr last)
@@ -278,10 +303,98 @@ protected:
 		blank_node->prev = blank_node;
 	}
 
+	void transfer(iterator position, iterator first, iterator last)
+	{
+		if(position != last)
+		{
+			first.node_->prev->next = last.node_;
+			last.node_->prev->next = position.node_;
+			position.node_->prev->next = first.node_;
+			list_node_ptr temp = position.node_->prev;
+			position.node_->prev = last.node_->prev;
+			last.node_->prev = first.node_->prev;
+			first.node_->prev = temp;
+		}
+	}
 
 private:
 	list_node_ptr blank_node;		// points to a blank node at the end of list
 };
 
+
+template <typename T, typename Alloc>
+void MyList<T, Alloc>::reverse()
+{
+	if(blank_node->next == blank_node || blank_node->next->next == blank_node) return;
+	iterator first = begin();
+	++first;
+	while(first != end())
+	{
+		iterator old = first;
+		++first;
+		transfer(begin(), old, first);
+	}
+}
+
+template <typename T, typename Alloc>
+void MyList<T, Alloc>::sort()
+{
+	if(blank_node->next == blank_node || blank_node->next->next == blank_node) return;
+	// quick sort
+	MyList<T, Alloc> carry;
+	MyList<T, Alloc> counter[64];
+	int fill = 0;
+	while(!empty())
+	{
+		carry.splice(carry.begin(), *this, begin());
+		int i = 0;
+		while(i < fill && !counter[i].empty())
+		{
+			counter[i].merge(carry);
+			carry.swap(counter[i]);
+			i++;
+		}
+		carry.swap(counter[i]);
+		if(i == fill) ++fill;
+	}
+
+	for(int i = 0; i != 64; ++i)
+	{
+		cout << "counter[" << i << "]: ";
+		for(auto iter = counter[i].begin(); iter != counter[i].end(); ++iter)
+			cout << *iter << " ";
+		cout << endl;
+	}
+	for(int i = 1; i < fill; ++i)
+		counter[i].merge(counter[i-1]);
+	swap(counter[fill-1]);
+}
+
+template <typename T, typename Alloc>
+void MyList<T, Alloc>::merge(MyList &x)
+{
+	iterator first1 = begin();
+	iterator last1 = end();
+	iterator first2 = x.begin();
+	iterator last2 = x.end();
+
+	while(first1 != last1 && first2 != last2)
+	{
+		if(*first2 < *first1)
+		{
+			iterator old = first2;
+			++first2;
+			transfer(first1, old, first2);
+		}
+		else
+		{
+			++first1;
+		}
+	}
+	if(first2 != last2)
+	{
+		transfer(last1, first2, last2);
+	}
+}
 
 #endif
